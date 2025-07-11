6b2d17300bfa36484fb8d7ad19744a8ed3334de9
tdissana <tharindudandissanayake@gmail.com>
2025-07-11 20:37:58 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-southeast-1_a7kJ9kA6j', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '66rgmqugu8hbsca1vkqhhuhedn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '7tne7oepvhf5dugt8m1pr6jvkb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
