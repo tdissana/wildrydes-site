548a05e2f33e2168bf0749a1f66b836afe61b499
tdissana <tharindudandissanayake@gmail.com>
2025-07-11 20:18:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_a7kJ9kA6j', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '66rgmqugu8hbsca1vkqhhuhedn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
